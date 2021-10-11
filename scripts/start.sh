d26a2d1ceda1c63d0b296d04645c0abe8b9a8c65
grahamfyfecodes <67826423+grahamfyfecodes@users.noreply.github.com>
2021-10-11 11:42:24 +0100

    Init Commit

---
+++ scripts/start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo 'Starting my app'
+cd '/tmp' || exit
+nohup java -jar idle-counter-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/com/fyfe/idlecounter/IdleCounterApplication.java
@@ -0,0 +1,13 @@
+package com.fyfe.idlecounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdleCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdleCounterApplication.class, args);
+	}
+
+}
